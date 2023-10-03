75db4a7f6235a638b25a1a713c1012475d445e5d
Hugo GALAN <hugo.galan8@gmail.com>
2023-10-03 03:47:18 +0200

    Updates

--- _app/immutable/chunks/paths.18593da2.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_11b83kj)==null?void 0:s.base)??"/EpitechFightersLeague";var e;const a=((e=globalThis.__sveltekit_11b83kj)==null?void 0:e.assets)??t;export{a,t as b};
